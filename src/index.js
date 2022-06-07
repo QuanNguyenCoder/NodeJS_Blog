96e42bb924f7935541a5f32d05918710a99a2fac
Tony Coder <quananhb2410@gmail.com>
2022-06-07 22:55:55 +0700

    refactor code: create routes, controllers

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const route = require('./routes');
+
 const app = express();
 const port = 3000;
 
 //Morgan loggers
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
+
+//middleware
+app.use(express.urlencoded({
+  extended: true,
+})); //Post method
+app.use(express.json()); //XMLHttpRequest, fetch, axios, etc
 
 //Static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Template engines
 app.engine('hbs', handlebars.engine({
@@ -19,12 +27,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+route(app);
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
